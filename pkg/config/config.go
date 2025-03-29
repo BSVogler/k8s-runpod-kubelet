03150c3f874d9678283c7ba8e0947f1a18d30fac
Benedikt S. Vogler <developer@benediktsvogler.com>
2025-03-29 11:10:13 +0100

    remove unused code

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -14,21 +14,10 @@ type Config struct {
 
 	// MaxPendingTime that a job is allowed to stay in pending state before it is offloaded
 	MaxPendingTime int
-	
+
 	// MaxGPUPrice is the maximum price per hour we're willing to pay for GPU instances
 	MaxGPUPrice float64
 
 	// HealthServerAddress is the address where the health server listens
 	HealthServerAddress string
-}
-
-// DefaultConfig returns a default configuration
-func DefaultConfig() Config {
-	return Config{
-		ReconcileInterval:   30 * time.Second,
-		PendingJobThreshold: 1,
-		MaxPendingTime:      0,
-		MaxGPUPrice:         0.5,
-		HealthServerAddress: ":8080",
-	}
 }
